bbf91cd0347e98197b3e80527419bd26ab969d46
LahiruAvishka <wlavishka.95@gmail.com>
2024-09-09 14:35:46 +0530

    Update

--- src/app/page/manage-emp/manage-emp.component.ts
+++ src/app/page/manage-emp/manage-emp.component.ts
@@ -1,12 +1,39 @@
+import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { first } from 'rxjs';
 
 @Component({
   selector: 'app-manage-emp',
   standalone: true,
-  imports: [],
+  imports: [FormsModule,HttpClientModule,CommonModule],
   templateUrl: './manage-emp.component.html',
   styleUrl: './manage-emp.component.css'
 })
 export class ManageEmpComponent {
+
+  public employeeObj ={
+    firstName:"",
+    lastName:"",
+    email:"",
+    departmentId:"",
+    roleId:""
+
+  }
+
+  constructor(private http:HttpClient){
+
+  }
+    addEmployee(){
+      this.http.post("http://localhost:8080/emp-controller/add-employee",this.employeeObj).subscribe(
+      (data) =>{
+        console.log(data);
+      }
+
+      )
+      
+    }
+
 
 }
