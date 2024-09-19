f5a3e624c9962f3543b571027334722c61ea0658
LahiruAvishka <wlavishka.95@gmail.com>
2024-09-19 11:01:00 +0530

    update

--- src/app/page/view-all-employee/view-all-employee.component.ts
+++ src/app/page/view-all-employee/view-all-employee.component.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-view-all-employee',
@@ -27,6 +28,39 @@ export class ViewAllEmployeeComponent {
   }
 
   deleteEmployee(employe:any) {
+    const swalWithBootstrapButtons = Swal.mixin({
+      customClass: {
+        confirmButton: "btn btn-success",
+        cancelButton: "btn btn-danger"
+      },
+      buttonsStyling: false
+    });
+    swalWithBootstrapButtons.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonText: "Yes, delete it!",
+      cancelButtonText: "No, cancel!",
+      reverseButtons: true
+    }).then((result) => {
+      if (result.isConfirmed) {
+        swalWithBootstrapButtons.fire({
+          title: "Deleted!",
+          text: "Your file has been deleted.",
+          icon: "success"
+        });
+      } else if (
+        /* Read more about handling dismissals below */
+        result.dismiss === Swal.DismissReason.cancel
+      ) {
+        swalWithBootstrapButtons.fire({
+          title: "Cancelled",
+          text: "Your imaginary file is safe :)",
+          icon: "error"
+        });
+      }
+    });
     this.http.delete(`http://localhost:8080/emp-controller/delete-emp/${employe.id}`,{responseType:'text'}).subscribe(res=>{
       this.loadEmployeeTable()
       console.log(res);
